a79a99b9c15c30911843ec04489f2c14ee4c10a4
Florence Suller <FlorenceSuller@gmail.com>
2018-05-24 10:11:48 +0800

    third commit

--- app/src/main/java/lifegiverappblog/lifelog/com/lifelog/fragments/HomeFragment.java
+++ app/src/main/java/lifegiverappblog/lifelog/com/lifelog/fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package lifegiverappblog.lifelog.com.lifelog.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import lifegiverappblog.lifelog.com.lifelog.R;
 public class HomeFragment extends Fragment {
 
 
+    private RecyclerView lifegroup_log_post_view;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -23,6 +25,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        lifegroup_log_post_view = getActivity().findViewById(R.id.post_view_lg_log);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
